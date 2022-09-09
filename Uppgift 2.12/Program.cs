e1bd6707b01b9c4e910871fcf0a9f4822c4235c2
E-Svensson <111962443+E-Svensson@users.noreply.github.com>
2022-09-09 11:02:56 +0200

    Add project files.

---
+++ Uppgift 2.12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Uppgift_2._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur länge ska du hyra bilen?");
+            string bilentext=Console.ReadLine();
+            int dagar=int.Parse(bilentext);
+            Console.WriteLine("Hur många kilometer ska du köra");   
+            int kilometer = int.Parse(Console.ReadLine());
+            int kostnad= 300 + 500 * (dagar - 1) + kilometer;
+            int summa = kilometer + kostnad;
+            int produkt = kilometer * kostnad;
+            Console.WriteLine("summan av talen är " + summa + " kr");
+            Console.WriteLine("produkten av talen är " + produkt);
+        }
+    }
+}
